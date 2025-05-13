570b41f3ced48954a1d311d0dbdc5bc2232e5818
MAXQGIT <83851714+MAXQGIT@users.noreply.github.com>
2025-05-13 16:57:01 +0800

    Add files via upload
    Signed-off-by: MAXQGIT <83851714+MAXQGIT@users.noreply.github.com>

--- build_neo4j.py
+++ build_neo4j.py
@@ -1,5 +1,7 @@
 from neo4j import GraphDatabase, RoutingControl
+import pandas as pd
 
+#neo4j console
 def delete_all(driver):
     driver.execute_query('MATCH (n) DETACH DELETE n')  # 删除数据库中所有数据
 
@@ -11,10 +13,10 @@ def create_code(driver, label, label_name, name):
 
 
 def create_code_relastion(driver, start_label_name, start_name, relation, end_label_name,
-                          end_name):
-    query = 'MERGE (start_label:%s {name: $start_name}) MERGE (end_label:%s {name: $end_name}) MERGE (start_label)-[:%s]->(end_label)' % (
+                          end_name,contact):
+    query = 'MERGE (start_label:%s {name: $start_name}) MERGE (end_label:%s {name: $end_name,contact:$contact}) MERGE (start_label)-[:%s]->(end_label)' % (
         start_label_name, end_label_name, relation)
-    driver.execute_query(query, start_name=start_name, end_name=end_name)
+    driver.execute_query(query, start_name=start_name, end_name=end_name,contact=contact)
 
 
 def create_relastion(driver, start_label_name, start_name, relation, end_label_name, end_name):
@@ -26,7 +28,7 @@ def create_relastion(driver, start_label_name, start_name, relation, end_label_n
 def query_information(start_label_name, start_name, konws, person):
     query = 'MATCH (start_label:%s)-[r:%s]->(end_label:%s) where start_label.name =$start_name  RETURN end_label.name' % (
         start_label_name, konws, person)
-    records, _, _ = driver.execute_query(query, start_name=start_name,database_="neo4j", routing_=RoutingControl.READ)
+    records, _, _ = driver.execute_query(query, start_name=start_name, database_="neo4j", routing_=RoutingControl.READ)
     return records
 
 
@@ -35,14 +37,21 @@ if __name__ == '__main__':
     auth = ('neo4j', '123456789')
     driver = GraphDatabase.driver(uri, auth=auth)
     delete_all(driver)  # 删除数据库中所有数据
-    for name in ["Guinevere", "Lancelot", "Merlin"]:
-        create_code_relastion(driver, 'person', 'Arthur',
-                              'knows', 'person', name)
-    create_relastion(driver, 'person', 'Guinevere',
-                     'like', 'person', 'Lancelot')
-    records = query_information('person', 'Arthur', 'knows', 'person')
-    for record in records:
-        print(record)
-        print(record['end_label.name'])
-        # print(record['end_label.name'])
-        print('~~'*50)
+    data = pd.read_excel('yewu.xlsx')
+    print(data)
+    for name,money,yewu,contact in zip(data['name'],data['money'],data['yewu'],data['contact']):
+        create_code_relastion(driver,'业务名称',name,'套餐','套餐名称',yewu,contact)
+        create_code_relastion(driver, '套餐名称', yewu,'费用','价格',money,'无')
+        # create_code_relastion(driver, '套餐名称', yewu, '内容', '套餐内容', contact)
+
+    # for name in ["Guinevere", "Lancelot", "Merlin"]:
+    #     create_code_relastion(driver, 'person', 'Arthur',
+    #                           'knows', 'person', name)
+    # create_relastion(driver, 'person', 'Guinevere',
+    #                  'like', 'person', 'Lancelot')
+    # records = query_information('person', 'Arthur', 'knows', 'person')
+    # for record in records:
+    #     print(record)
+    #     print(record['end_label.name'])
+    #     # print(record['end_label.name'])
+    #     print('~~'*50)
